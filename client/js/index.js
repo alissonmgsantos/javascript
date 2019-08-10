f88079d2509b50ddcbebe2a3eb3e7896d09945d5
Alisson Matos <alissonmgsantos@gmail.com>
2019-08-10 09:01:19 -0300

    Populando tabela

--- client/js/index.js
+++ client/js/index.js
@@ -5,13 +5,29 @@ let campos = [
 ];
 
 /* Populando a tabela após o submit do formulário */
+let tbody = document.querySelector('table tbody');
 document.querySelector('.form').addEventListener('submit', function (event) {
+
+    // Evitando que o formulário seja submetido
+    event.preventDefault();
+
     let tr = document.createElement('tr');
     campos.forEach(campo => {
         let td = document.createElement('td');
             td.textContent = campo.value;
             tr.appendChild(td)
     });
+    
 
-    let tdVolume = document.creat
+    let tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.append(tdVolume);
+    tbody.appendChild(tr);
+
+    // Limpenado os campos da tela
+
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+    campos[0].focus();
 });
