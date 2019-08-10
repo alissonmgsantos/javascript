5ff842533aeeb1a2297b3c3f5cc43f4b3aca4dc9
Alisson Matos <alissonmgsantos@gmail.com>
2019-08-10 10:39:42 -0300

    Get os valores do formulário e passando para class

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,14 +2,26 @@ class NegociacaoController {
 
     constructor() {
         let $ = document.querySelector.bind(document); // Tornando a chamada do document.querySelector mais clear
-        this.inputData = $('#data');
-        this.inputQunatidade = $('#quantidade');
-        this.inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQunatidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
 
     adiciona(event) {
         event.preventDefault();
-       console.log( this.inputData.value);
-       
+        // Passando o valor da data da forma correta para a classe Negociacao
+        let data = new Date(
+            ...this._inputData.value.split('-')
+                .map((item, indice) => item - indice % 2) //Arrow function
+        )
+
+        // Passando os dados do formulário para criação da negociação
+        let negociacao = new Negociacao(
+            data,
+            this._inputQunatidade.value,
+            this._inputValor.value
+        )
+            console.log(negociacao);
+            
     }
 }
