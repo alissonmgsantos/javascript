412cde68d386677935f318e5ea1f1fd15b54ecfb
Alisson Matos <alissonmgsantos@gmail.com>
2019-08-28 14:58:39 -0300

    Simplificando as requisições ajax com fetch

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -10,21 +10,26 @@
 class HttpService {
 
     get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        console.log(xhr.responseText);
-                        reject(xhr.responseText);
-                    }
-                }
-            }
+        return fetch(url)
+        .then(res => this._handleErrors(res))
+        .then(res => res.json());
+    }
 
-            xhr.send();
-        });
+    post(url, dado) {
+
+        return fetch(url, {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res));
+}
+
+    _handleErrors(res) {
+        if(res.ok) {
+            return res;
+        } else {
+          throw new Error(res.statusText);
+        }
     }
 }
